3a378a4369872110829a596edaf8b39004cb2df1
Harish Marimuthu <hmarimuthu@companieshouse.gov.uk>
2024-12-30 11:48:04 +0000

    SEC-155 Lb listener default rule to port 443

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -36,16 +36,14 @@ module "file_transfer_alb" {
   ingress_cidrs             = local.ingress_cidrs_private
   ingress_prefix_list_ids   = local.ingress_prefix_list_ids
   service_configuration     = {
-    default = {
-      listener_config = {
+    listener_config         = {
         default_action_type = "fixed-response"
         protocol            = "HTTPS"
         port                = 443
         fixed_response      = {
-          status_code = 404
+          status_code       = 404
         }
       }
-    }
   }
 }
 
